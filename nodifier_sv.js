f1aeb673894d115139727e5621520e3a7067c078
Rasmus Eskola <fruitiex@gmail.com>
2014-02-18 23:36:58 +0200

    make copy of notification text before shortening it...

--- nodifier_sv.js
+++ nodifier_sv.js
@@ -125,11 +125,12 @@ var drawNotification = function(notification) {
 
 	var pos_string = notification.id.toString();
 
+	// make a copy of the string before we potentially shorten
+	var text = notification.text;
 	// if the string is wider than our terminal we need to shorten it
 	var source_text_length = 5 + pos_string.length + notification.source.length + date_string.length;
-	var text_length = notification.text.length;
-	if(source_text_length + text_length > process.stdout.columns)
-		notification.text = notification.text.substr(0, process.stdout.columns - source_text_length - 3) + '...';
+	if(source_text_length + text.length > process.stdout.columns)
+		text = text.substr(0, process.stdout.columns - source_text_length - 3) + '...';
 
 	console.log(clc_color.date_color(date_string) + clc_color.id_color(' ' + pos_string + ' ') + source_color(' ' + notification.source + ' ') + ' ' + notification.text);
 };
