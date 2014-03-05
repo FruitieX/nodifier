d3b5f15acf8b8bbfd7e87d052de6ad53f94d4847
Rasmus Eskola <fruitiex@gmail.com>
2014-03-06 01:17:43 +0200

    remove old comments and restructure a few things

--- plugins/mail/plugin.js
+++ plugins/mail/plugin.js
@@ -8,29 +8,6 @@ var crypto = require('crypto');
 var EventEmitter = require('events').EventEmitter;
 var post = require('../../lib/post.js');
 
-// most of the mail handling is done via a slightly modified version of
-// the mail-notifier library by Jerome Creignou:
-// https://github.com/jcreigno/nodejs-mail-notifier
-
-function dec2hex(str) { // .toString(16) only works up to 2^53
-    var dec = str.toString().split(''), sum = [], hex = [], i, s;
-
-    while(dec.length) {
-        s = 1 * dec.shift();
-        for(i = 0; s || i < sum.length; i++) {
-            s += (sum[i] || 0) * 10;
-            sum[i] = s % 16;
-            s = (s - sum[i]) / 16;
-        }
-    }
-
-    while(sum.length) {
-        hex.push(sum.pop().toString(16));
-    }
-
-    return hex.join('');
-}
-
 exports.start = function(config) {
 	var imap = new Imap({
 		user: config.user,
@@ -43,13 +20,31 @@ exports.start = function(config) {
 		//debug: console.log
 	});
 
+	function dec2hex(str) { // .toString(16) only works up to 2^53
+		var dec = str.toString().split(''), sum = [], hex = [], i, s;
+
+		while(dec.length) {
+			s = 1 * dec.shift();
+			for(i = 0; s || i < sum.length; i++) {
+				s += (sum[i] || 0) * 10;
+				sum[i] = s % 16;
+				s = (s - sum[i]) / 16;
+			}
+		}
+
+		while(sum.length) {
+			hex.push(sum.pop().toString(16));
+		}
+
+		return hex.join('');
+	}
+
 	var unread = {};
 
 	// new unread mail arrived
 	var newUnread = function(from, subject, uid, threadId, labels) {
 		var text = from + ', Subject: "' + subject + '"';
 		var hash = crypto.randomBytes(20).toString('hex');
-		console.log(hash);
 
 		unread[hash] = uid;
 
@@ -61,7 +56,8 @@ exports.start = function(config) {
 			'app': config.app,
 			'url': 'https://mail.google.com/mail/u/0/#inbox/' + threadId,
 			'colorbg': config.colorbg,
-			'colorfg': config.colorfg
+			'colorfg': config.colorfg,
+			'response_addr': config.response_addr
 		});
 	};
 
@@ -79,17 +75,12 @@ exports.start = function(config) {
 
 	var next_uid = 1;
 	var searchUnseen = function(firstrun) {
-		console.log('searching: ' + next_uid);
 		imap.search(['UNSEEN', next_uid.toString() + ':*'], function(err, results) {
-			console.log(results);
 			if(err) throw err;
 			if(results.length) {
 				var f = imap.fetch(results, { bodies: '' });
 				f.on('message', function(msg, seqno) {
-					//console.log('Message #%d', seqno);
-					//inspect(msg);
-					console.log(msg);
-					var prefix = '(#' + seqno +') ';
+					//var prefix = '(#' + seqno +') ';
 
 					var from, subject, uid, threadId, labels;
 					msg.on('body', function(stream, info) {
@@ -111,7 +102,6 @@ exports.start = function(config) {
 						labels = attrs['x-gm-labels'];
 					});
 					msg.once('end', function() {
-						console.log(prefix + 'Finished');
 						newUnread(from, subject, uid, threadId, labels);
 					});
 				});
@@ -137,39 +127,6 @@ exports.start = function(config) {
 				console.log('expunge ', seqno);
 			});
 		});
-			/*
-			if (err) throw err;
-			var f = imap.seq.fetch('1:3', {
-				bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-				struct: true
-			});
-			f.on('message', function(msg, seqno) {
-				console.log('msg #%d', seqno);
-				var prefix = '(#' + seqno + ') ';
-				msg.on('body', function(stream, info) {
-					var buffer = '';
-					stream.on('data', function(chunk) {
-						buffer += chunk.toString('utf8');
-					});
-					stream.once('end', function() {
-						console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-					});
-				});
-				msg.once('attributes', function(attrs) {
-					console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-				});
-				msg.once('end', function() {
-					console.log(prefix + 'Finished');
-				});
-			});
-			f.once('error', function(err) {
-				console.log('Fetch error: ' + err);
-			});
-			f.once('end', function() {
-				console.log('Done fetching all messages!');
-				imap.end();
-			});
-			*/
 	});
 
 	imap.once('error', function(err) {
@@ -181,105 +138,3 @@ exports.start = function(config) {
 	});
 	imap.connect();
 };
-			/*
-			.seq(function(){ self.imap.connect(this); })
-			.seq(function(){
-				self.connected = true;
-				self.imap.openBox('INBOX',true,this);
-			}).seq(function(){
-				util.log(config.source + ' - successfully opened mail box');
-				self.imap.on('mail', function(id){ self.scan(); });
-				self.imap.on('update', function(id) { console.log(id); });
-				self.scan();
-				if(!next_uid)
-					next_uid = self.imap._state.box._uidnext;
-			});
-			*/
-
-	/*
-	Notifier.prototype.scan = function(){
-		var self = this;
-		Seq()
-			.seq(function(){
-				self.imap.search(['UNSEEN'],this);
-			})
-		.seq(function(searchResults){
-			var i;
-
-			// remove all messages up to next_uid
-			for (i = 0; i < searchResults.length; i++) {
-				if(searchResults[i] < next_uid) {
-					searchResults.splice(i--, 1);
-				}
-			}
-
-			// get maximum uid and set next_uid accordingly
-			for (i = 0; i < searchResults.length; i++) {
-				if(searchResults[i] >= next_uid)
-					next_uid = searchResults[i] + 1;
-			}
-
-			if(!searchResults || searchResults.length === 0) {
-				util.log(config.source + ' - no new mail in INBOX');
-				return;
-			} else {
-				var fetch = self.imap.fetch(searchResults, {
-					'bodies': 'HEADER.FIELDS (FROM TO SUBJECT DATE)'
-				});
-				fetch.on('message', function(msg) {
-					var mp = new MailParser();
-					mp.on('end',function(mail){
-						self.emit('mail',mail);
-					});
-					msg.on('data', function(chunk) {
-						console.log(chunk.toString());
-						mp.write(chunk.toString());
-					});
-					msg.on('end', function() {
-						mp.end();
-					});
-				});
-				fetch.on('end', function() {
-					util.log(config.source + ' - Done fetching all messages!');
-				});
-			}
-		});
-		return this;
-	};
-
-	Notifier.prototype.stop = function(){
-		if(this.connected){
-			this.imap.logout();
-		}
-		util.log(config.source + ' - mail box closed.');
-		return this;
-	};
-
-	var setupListener = function(ml) {
-		mailListener.on('end', function() { // reconnect
-			mailListener.stop();
-			mailListener = new Notifier(config);
-			setupListener(mailListener);
-		});
-
-		mailListener.on('mail', function(mail) {
-			var subject = mail.subject;
-			var text = mail.headers.from + ', Subject: "' + subject + '"';
-			util.log(config.source + ' - new mail: ' + text);
-			post.sendPOST({
-				'method': 'newNotification',
-				'uid': config.source + mail.headers['message-id'],
-				'text': text,
-				'source': config.source,
-				'app': config.app,
-				'url': config.url,
-				'colorbg': config.colorbg,
-				'colorfg': config.colorfg
-			});
-		}).start();
-	};
-
-	var mailListener = new Notifier(config);
-
-	setupListener(mailListener);
-	*/
