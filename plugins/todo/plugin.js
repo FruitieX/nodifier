faefe80404f2a796caf69b586205ba86c7322502
Rasmus Eskola <fruitiex@gmail.com>
2014-03-06 14:36:58 +0200

    todo plugin for quick todos

---
+++ plugins/todo/plugin.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+// todo plugin for quick adding of todos
+var post = require('../../lib/post.js');
+
+if(!process.argv[2]) {
+	console.log("Usage: todo [message]");
+	process.exit(1);
+}
+
+var str = "";
+for (var i = 2; i < process.argv.length; i++) {
+	str += process.argv[i] + ' ';
+}
+str.substring(0, str.length - 1);
+
+post.sendPOST({
+	'method': 'newNotification',
+	'text': str,
+	'source': 'todo',
+	'colorbg': 'blue',
+	'colorfg': 'black'
+});
