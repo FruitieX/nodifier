93cb7b88168cede15ba0db5c85b3a04b30f67724
Rasmus Eskola <fruitiex@gmail.com>
2014-03-07 03:11:14 +0200

    mail plugin rewrite AGAIN, now all issues should be tackled

--- plugins/mail/plugin.js
+++ plugins/mail/plugin.js
@@ -38,19 +38,15 @@ exports.start = function(config) {
 		return hex.join('');
 	}
 
-	var unread = {};
-	var read = {};
+	var unread = [];
 
 	// new unread mail arrived
 	var newUnread = function(from, subject, uid, threadId, labels) {
 		var text = from + ', Subject: "' + subject + '"';
-		var hash = crypto.randomBytes(20).toString('hex');
 
-		unread[hash] = uid;
-
-		post.sendPOST({
+		var notification = {
 			'method': 'newNotification',
-			'uid': hash,
+			'uid': uid,
 			'text': text,
 			'source': config.source,
 			'app': config.app,
@@ -59,94 +55,119 @@ exports.start = function(config) {
 			'colorfg': config.colorfg,
 			'response_host': config.response_host,
 			'response_port': config.response_port
-		});
+		};
+
+		unread.push(uid);
+
+		post.sendPOST(notification);
 	};
 
-	// mark mail as read, then forget about it
-	var setRead = function(hash) {
-		var uid = unread[hash];
-		if(uid) {
-			imap.setFlags([uid], 'SEEN', function(err) {
-				if(err) throw err;
-				console.log("Set message with UID " + uid + " as read.");
-				read[hash] = unread[hash];
-				delete unread[hash];
-			});
-		}
-	};
-
-	// mark mail as unread
-	var setUnread = function(hash) {
-		var uid = read[hash];
-		if(uid) {
-			imap.delFlags([uid], 'SEEN', function(err) {
-				if(err) throw err;
-				console.log("Set message with UID " + uid + " as unread.");
-				unread[hash] = read[hash];
-				delete read[hash];
-			});
-		}
-	};
-
-	/* Sync (un)read message statuses with IMAP server
-	 * Because gmail servers don't push updates :(
-	 */
-	var syncUnread = function() {
-		imap.search(['UNSEEN'], function(err, imap_unread) {
+	// notify server wants to mark mail as read
+	var setRead = function(uid) {
+		imap.setFlags([uid], 'SEEN', function(err) {
 			if(err) throw err;
-			//console.log('syncresults: ' + results);
-			for(var hash in unread) {
-				// currently only handle marking as read, marking as
-				// unread is hard because we might have deleted a
-				// notification on the server! TODO
-				if(imap_unread.indexOf(unread[hash]) === -1) {
-					post.sendPOST({
-						'method': 'setRead',
-						'uid': hash,
-						'noSendResponse': true
-					});
-					read[hash] = unread[hash];
-					delete unread[hash];
-				}
+			console.log("Set message with UID " + uid + " as read.");
+
+			if(unread.indexOf(uid) !== -1) {
+				unread.splice(unread.indexOf(uid), 1);
+			} else {
+				console.log("ERROR: Notify server tried to mark non existing UID as read!");
 			}
 		});
 	};
 
-	/* Searches for new messages from next_uid:*
-	 * Updates next_uid if new messages were found */
-
-	var next_uid = 1;
-	var searchNew = function(firstrun) {
-		imap.search(['UNSEEN', next_uid.toString() + ':*'], function(err, results) {
+	// notify server wants to mark mail as unread
+	var setUnread = function(uid) {
+		imap.delFlags([uid], 'SEEN', function(err) {
 			if(err) throw err;
-			if(results.length) {
-				var f = imap.fetch(results, { bodies: '' });
-				f.on('message', function(msg, seqno) {
-					//var prefix = '(#' + seqno +') ';
+			console.log("Set message with UID " + uid + " as unread.");
 
-					var from, subject, uid, threadId, labels;
-					msg.on('body', function(stream, info) {
-						var buffer = '';
-						stream.on('data', function(chunk) {
-							buffer += chunk.toString('utf8');
-						});
-						stream.on('end', function() {
-							var header = Imap.parseHeader(buffer);
-							from = header.from;
-							subject = header.subject;
-						});
+			if(unread.indexOf(uid) === -1) {
+				unread.push(uid);
+			} else {
+				console.log("ERROR: Notify server tried to mark non existing UID as unread!");
+			}
+		});
+	};
+
+	/* fetch msgs and mark them as new notifications */
+	var fetchAndNotify = function(msgs) {
+		if(msgs.length) {
+			var f = imap.fetch(msgs, { bodies: '' });
+			f.on('message', function(msgs, seqno) {
+				//var prefix = '(#' + seqno +') ';
+
+				var from, subject, uid, threadId, labels;
+				msgs.on('body', function(stream, info) {
+					var buffer = '';
+					stream.on('data', function(chunk) {
+						buffer += chunk.toString('utf8');
 					});
-					msg.once('attributes', function(attrs) {
-						if(attrs.uid >= next_uid)
-							next_uid = attrs.uid + 1;
-						uid = attrs.uid;
-						threadId = dec2hex(attrs['x-gm-thrid']);
-						labels = attrs['x-gm-labels'];
-					});
-					msg.once('end', function() {
-						newUnread(from, subject, uid, threadId, labels);
+					stream.on('end', function() {
+						var header = Imap.parseHeader(buffer);
+						from = header.from;
+						subject = header.subject;
 					});
 				});
+				msgs.once('attributes', function(attrs) {
+					uid = attrs.uid;
+					threadId = dec2hex(attrs['x-gm-thrid']);
+					labels = attrs['x-gm-labels'];
+				});
+				msgs.once('end', function() {
+					newUnread(from, subject, uid, threadId, labels);
+				});
+			});
+		}
+	};
+
+	/* Remove all previously seen UIDs from msgs */
+	/*
+	var removeOld = function(msgs) {
+		for(var uid in unread) {
+			var msg_index = msgs.indexOf(uid);
+			if(msg_index === -1) {
+				msgs.splice(msg_index, 1);
+			}
+		}
+	};
+	*/
+
+	/*
+	 * IMAP server state has changed, sync plugin and notify server accordingly
+	 * Ran each time a new mail arrives and also at a set interval
+	 * Interval due to gmail servers not sending updates for flag changes :(
+	 */
+	var syncFromIMAP = function() {
+		imap.search(['UNSEEN'], function(err, results) {
+			if(err) throw err;
+			var i;
+
+			// search for new unread mail
+			// either unseen before or marked as read on notify server
+			// (both of these require the mail notification be resent)
+			var newUnread = [];
+			for(i in results) {
+				// found a mail not in our unread array
+				if(unread.indexOf(results[i]) === -1) {
+					newUnread.push(results[i]);
+				}
+			}
+			fetchAndNotify(newUnread);
+
+			// now do this vice-versa:
+			// search for mails that we have marked as unread, but IMAP
+			// server has marked as read. get rid of these notifications
+			for(i in unread) {
+				// found a mail not in the search results
+				if(results.indexOf(unread[i]) === -1) {
+					post.sendPOST({
+						'method': 'setRead',
+						'uid': unread[i],
+						'noSendResponse': true
+					});
+					unread.splice(i, 1);
+				}
 			}
 		});
 	};
@@ -155,13 +176,9 @@ exports.start = function(config) {
 		imap.openBox('INBOX', false, function(err, box) {
 			console.log('box opened.');
 
-			// if this is a reconnect, don't reset next_uid
-			if(next_uid == 1)
-				next_uid = box.uidnext;
-
 			imap.on('mail', function(numNewMsgs) {
 				console.log('new mail ', inspect(numNewMsgs));
-				searchNew(imap);
+				syncFromIMAP();
 			});
 
 			imap.on('update', function(seqno, info) {
@@ -173,44 +190,21 @@ exports.start = function(config) {
 			});
 
 			setInterval(function() {
-				syncUnread();
+				syncFromIMAP();
 			}, config.unreadSyncInterval * 1000);
 		});
 	});
 
-	imap.once('error', function(err) {
+	imap.on('error', function(err) {
 		console.log(err);
 	});
 
-	imap.once('end', function() {
+	imap.on('end', function() {
 		console.log('Connection ended, reconnecting...');
 		imap.connect();
 	});
+
 	imap.connect();
-
-	var url_read = /(.*read)\/(.*)/;
-	var handleGET = function(req, res) {
-		var resource = url.parse(req.url).pathname;
-		resource = resource.substr(1);
-
-		var url_matches = resource.match(url_read);
-
-		var hash;
-		if(url_matches[1] == 'read') {
-			hash = url_matches[2];
-			setRead(hash);
-		} else if (url_matches[1] == 'unread') {
-			hash = url_matches[2];
-			setUnread(hash);
-		}
-
-		var msg = "ok";
-		res.writeHead(200, msg, {
-			'Content-Type': 'text/html',
-			'Content-Length': Buffer.byteLength(msg, 'utf8')
-		});
-		res.end(msg);
-	};
 
 	/* HTTP server for reporting read/unread statuses to plugin */
 	var http = require('http');
@@ -225,5 +219,30 @@ exports.start = function(config) {
 	s = http.createServer(basic, function (req, res) {
 		handleGET(req, res);
 	});
+
 	s.listen(config.response_port);
+
+	var url_read = /(.*read)\/(.*)/;
+	var handleGET = function(req, res) {
+		var resource = url.parse(req.url).pathname;
+		resource = resource.substr(1);
+
+		var url_matches = resource.match(url_read);
+
+		var uid;
+		if(url_matches[1] == 'read') {
+			uid = url_matches[2];
+			setRead(uid);
+		} else if (url_matches[1] == 'unread') {
+			uid = url_matches[2];
+			setUnread(uid);
+		}
+
+		var msg = "ok";
+		res.writeHead(200, msg, {
+			'Content-Type': 'text/html',
+			'Content-Length': Buffer.byteLength(msg, 'utf8')
+		});
+		res.end(msg);
+	};
 };
