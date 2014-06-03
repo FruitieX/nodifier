5ca8caac302175f6ebd070dfd0e207d131723959
Rasmus Eskola <fruitiex@gmail.com>
2014-06-04 00:53:39 +0300

    get rid of post plugin entirely

--- plugins/spam/plugin.js
+++ plugins/spam/plugin.js
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
 
 // spam plugin, useful for testing
-var socket = require('../../lib/socketio-send.js');
+var config = require('../../config.json');
+var socket = require('socket.io-client')(config.host + ':' + config.port);
 
 var cnt = 0;
-setInterval(function() {
-	socket.send({
-		'method': 'newNotification',
-		'text': 'spamäåö123456789atheoutheachumcramcrhkrcehachuechacmecuaocemuchaechucehaocumechoaceuhcmkch.phehlowhwhell' + cnt,
-		'source': 'source' + cnt,
-		'sourcebg': 'red',
-		'sourcefg': 'white',
-		'context': 'context' + cnt,
-		'contextbg': 'blue',
-		'contextfg': 'white',
-		'openwith': 'openwith' + cnt,
-		'url': 'url' + cnt
-	});
-	cnt++;
-}, 200);
+socket.on('connect', function() {
+	setInterval(function() {
+		socket.emit('newNotification', {
+			'method': 'newNotification',
+			'text': 'spamäåö123456789atheoutheachumcramcrhkrcehachuechacmecuaocemuchaechucehaocumechoaceuhcmkch.phehlowhwhell' + cnt,
+			'source': 'source' + cnt,
+			'sourcebg': 'red',
+			'sourcefg': 'white',
+			'context': 'context' + cnt,
+			'contextbg': 'blue',
+			'contextfg': 'white',
+			'openwith': 'openwith' + cnt,
+			'url': 'url' + cnt
+		});
+		cnt++;
+	}, 200);
+});
