b39c1135fbc1eb3ac28bbb3997e597cefe0b63ad
Rasmus Eskola <fruitiex@gmail.com>
2014-06-05 15:55:47 +0300

    WIP: template client

---
+++ plugins/template/plugin.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+// template client program
+var config = require('../../config.json');
+var socket = require('socket.io-client')(config.host + ':' + config.port);
+
+socket.on('newNotification', function(notification) {
+	// new notification arrived, print text property
+	console.log('new notification: ' + notification.source + ': ' + notification.text);
+});
+socket.on('markAs', function(notifications) {
+	// existing notifications' read property changed, print the old indices
+	for (var i = 0; i < notifications.length; i++) {
+		if(notifications[i].read)
+			console.log('notification ' + notifications[i].unreadID + ' marked as read.');
+		else
+			console.log('notification ' + notifications[i].readID + ' marked as unread.');
+	}
+});
+socket.on('notifications', function(notifications) {
+	// server sent back a list of notifications that you've requested
+	console.log('got list of notifications:');
+	for (var i = 0; i < notifications.length; i++)
+		console.log(notifications[i].source + ': ' + notifications[i].text)
+});
+socket.on('connect', function() {
+	socket.emit('newNotification', {
+		'text': 'notification text goes here',
+		'source': 'testapp',
+		'sourcebg': 'blue',
+		'sourcefg': 'black'
+	});
+	socket.emit('markAs', {
+		'read': true,
+		'source': 'testapp'
+	});
+	socket.emit('getRead');
+	socket.emit('getUnread', {
+		'id': '5..42'
+	});
+});
