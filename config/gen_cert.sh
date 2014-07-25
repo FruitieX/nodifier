a11a241453e25fd3de7a1fa92e6a2b4cda3b8fe9
FruitieX <fruitiex@gmail.com>
2014-07-25 16:25:04 +0300

    default certificate validity > 30 days thankyou

--- config/gen_cert.sh
+++ config/gen_cert.sh
@@ -11,4 +11,4 @@ CERTNAME=$NAME-cert.pem
 
 openssl genrsa -out $KEYNAME 1024
 openssl req -new -key $KEYNAME -out $CSRNAME
-openssl x509 -req -in $CSRNAME -signkey $KEYNAME -out $CERTNAME
+openssl x509 -req -days 10000 -in $CSRNAME -signkey $KEYNAME -out $CERTNAME
